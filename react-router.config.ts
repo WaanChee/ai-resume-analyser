aae6a100658d20c1f0f80ee9effd83a8d25a113e
Chan <waanchee@gmail.com>
2026-06-19 13:49:00 +0800

    Authentication

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,13 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  async getInlineFieldType(source: string) {
+    return undefined;
+  },
+  // Build configuration for static assets
+  buildDirectory: "build",
+  assetsBuildDirectory: "build/client/assets",
+  publicPath: "/",
   future: {
     v8_middleware: true,
     v8_passThroughRequests: true,
